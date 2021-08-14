256c9d73d28be718cbd9d0063018932c321f908d
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-08-15 06:13:56 +0800

    update keys

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,9 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   // 我們在 production - return the prod set of keys
   module.exports = require('./prod');
-  console.log('now is in prod')
 } else {
   // 我們在 development - return the dev keys!!
   module.exports = require('./dev');
-  console.log('now is in dev')
 }
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,6 @@
 // 上傳用密鑰
 module.exports = {
-  emailjsServicesID: process.env.EMAILJS_SERVICES_ID,
-  emailjsTemplateID: process.env.EMAILJS_TEMPLATE_ID,
-  emailjsUserID: process.env.EMAILJS_USER_ID,
+  emailjsServicesID: process.env.REACT_APP_EMAILJS_SERVICES_ID,
+  emailjsTemplateID: process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+  emailjsUserID: process.env.REACT_APP_EMAILJS_USER_ID,
 };
