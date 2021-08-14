2c159087b44e993286f8fbab748841e104294bae
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-08-14 15:48:12 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,10 @@ class App extends React.Component {
     return (
       <div className="p-2 lg:p-5 text-blue-900 w-full h-full overflow-y-auto relative" >
         <div className="fixed top-0 w-full pt-2 lg:pt-5 bg-white z-20" />
-        123
-        {/* <Header />
+        <Header />
         <Home />
         <Footer />
-        <GuideButton /> */}
+        <GuideButton />
         <div className="fixed bottom-0 w-full pb-2 lg:pb-5 bg-white z-20" />
       </div>
     )
